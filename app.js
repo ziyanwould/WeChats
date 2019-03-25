3f6b5394cae3277709dcb10663e79e3059239161
ziyanwould <448736378@qq.com>
2019-03-25 09:43:20 +0800

    公总号相关功能完成

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ let tpl = heredoc(()=>{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>猜电影</title>
+    <title>搜索电影</title>
 </head>
 
 <body>
@@ -29,25 +29,130 @@ let tpl = heredoc(()=>{
     <p class="title">
 
     </p>
+    <div class="directors">
+
+    </div>
+    <div class="year">
+
+    </div>
+
     <div class="poster">
 
     </div>
-    <script src="https://cdn.bootcss.com/jquery/2.0.0/jquery.js"></script>
+    <script src="https://libs.baidu.com/jquery/2.0.0/jquery.min.js"></script>
     <script src="https://res2.wx.qq.com/open/js/jweixin-1.4.0.js"></script>
     <script>
         wx.config({
-            debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+            debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: 'wxf2d1d23f5191f54a', // 必填，公众号的唯一标识
             timestamp: '<%= timestamp %>', // 必填，生成签名的时间戳
             nonceStr: '<%= noncestr %>', // 必填，生成签名的随机串
             signature: '<%= signature %>', // 必填，签名
             jsApiList: [
-            'startRecord',
-            'stopRecord',
-            'onVoiceRecordEnd',
-            'translateVoice'
-            ] // 必填，需要使用的JS接口列表
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'onMenuShareQZone',
+                    'startRecord',
+                    'stopRecord',
+                    'onVoiceRecordEnd',
+                    'translateVoice',
+                    ''
+                ] // 必填，需要使用的JS接口列表
         });
+        wx.ready(function() {
+            wx.checkJsApi({
+                jsApiList: ['onVoiceRecordEnd'],
+                success: function(res) {
+                    console.log(res)
+
+                }
+            });
+            let shareContent = {
+                title: '分享标题', // 分享标题
+                desc: '分享描述', // 分享描述
+                link: 'https:yln212.top', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                imgUrl: 'https://yln212.top/-M_w587G8t-qPtaRdCALx.png', // 分享图标
+                type: 'link', // 分享类型,music、video或link，不填默认为link
+                dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+                success: function() {
+                    alert(分享成功)
+                },
+                cancel: () => {
+                    alert(分享失败)
+                }
+            }
+            wx.onMenuShareAppMessage(shareContent)
+            let vido = false
+            $('h1').click(function() {
+                if (!vido) {
+                    vido = true
+                    wx.startRecord({
+                        cancel: () => {
+                            alert('那就不去搜索了')
+                        }
+                    });
+
+                    return
+                }
+
+                vido = false
+
+                wx.stopRecord({
+                    success: function(res) {
+                        var localId = res.localId;
+
+                        wx.translateVoice({
+                            localId: localId,
+                            isShowProgressTips: 1,
+                            success: function(res) {
+                                // alert(res.translateResult); // 语音识别的结果
+                                $.ajax({
+                                    url: "https://api.douban.com/v2/movie/search?q=流浪地球", //res.translateResult,
+                                    type: "GET",
+                                    dataType: "jsonp", //指定服务器返回的数据类型
+                                    success: function(data) {
+                                        let message = data.subjects[0]
+                                        $('.title').html(message.title)
+                                        $('.directors').html(message.directors[0].name)
+                                        $('.year').html(message.year)
+                                        $('.poster').html(` <img src="https://yln212.top/-M_w587G8t-qPtaRdCALx.png" alt="" class="">`)
+
+                                        shareContent = {
+                                            title: message.title, // 分享标题
+                                            desc: '你懂的', // 分享描述
+                                            link: 'https://github.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+                                            imgUrl: 'https://yln212.top/-M_w587G8t-qPtaRdCALx.png', // 分享图标
+                                            success: () => {
+                                                alert(分享成功)
+                                            },
+                                            cancel: () => {
+                                                alert(分享失败)
+                                            }
+                                        }
+
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            })
+        });
+
+        $('.poster').click(function() {
+            wx.previewImage({
+                current: 'https://yln212.top/-M_w587G8t-qPtaRdCALx.png', // 当前显示图片的http链接
+                urls: [
+                        'https://yln212.top/-NmoO5FsfAPjOBGa-saLg.jpg',
+                        'https://yln212.top/-SFEFYo1prGn45p7t2ud0.png',
+                        'https://yln212.top/1Umaa1p5fbP6oHdkS2mFR.png',
+                        'https://yln212.top/1vCVN3VmPHuyFvyvWYN2O.jpg',
+                        'https://yln212.top/2C1Dfsh4mfOhCqTgg3dW6.png'
+                    ] // 需要预览的图片http链接列表
+            });
+        })
     </script>
 </body>
 
